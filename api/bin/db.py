1eaba1ac5c83854649b5d61a38be3d65ae2e0d44
Gaffer Fitch <gaffer.fitch@gmail.com>
2020-08-15 11:58:40 -0400

    Logging for DB

--- api/bin/db.py
+++ api/bin/db.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 
+import klotio
 import nandyio_people_models
+
+logger = klotio.logger('nandy-io-people-db')
+
+logger.debug("init", extra={
+    "init": {
+        "mysql": {
+            "database": nandyio_people_models.MySQL.DATABASE
+        }
+    }
+})
 
 nandyio_people_models.MySQL.create_database()
 nandyio_people_models.MySQL.Base.metadata.create_all(nandyio_people_models.MySQL().engine)
